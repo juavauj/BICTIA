84a499ea3e3c7b32855776522beb6e6ecb0502b9
Juan Castaneda <juavauj@gmail.com>
2020-01-21 11:59:26 -0500

    Agregué un archivo javascript

---
+++ Repaso Bictia 21-01-2020/javascript.js
@@ -0,0 +1,24 @@
+// alert("Quiubo perro");
+
+console.log("Hola mi perro");
+
+console.log("Lorem");
+
+var idElemento = document.getElementById("idTest").innerHTML = "Uno";
+
+var idElemento2 = document.getElementById("idTest2").innerHTML = "Dos"
+
+var idElemento2 = document.getElementById("idTest3").innerHTML = "Tres"
+
+var idElemento2 = document.getElementById("idTest4").innerHTML = "Cuatro"
+
+var idElemento = document.getElementById("idTest5").innerHTML = "Cinco";
+
+var idElemento2 = document.getElementById("idTest6").innerHTML = "Seis"
+
+var idElemento2 = document.getElementById("idTest7").innerHTML = "Siete"
+
+var idElemento2 = document.getElementById("idTest8").innerHTML = "Ocho"
+
+
+console.log(idElemento);
