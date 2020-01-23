57fbee3bedb470f5b801403f46d42d2934e218b9
Juan Castaneda <juavauj@gmail.com>
2020-01-23 10:51:53 -0500

    Nuevo termino 'for' y un pequeño ejemplo

---
+++ Bictia 23-01-2020/javascript.js
@@ -0,0 +1,13 @@
+function cambiarTitulo(id, otromensaje) {
+    document.getElementById(id).innerHTML = otromensaje;
+}
+
+for (var i = 0; i <= 10; i++)
+
+
+
+/*function cambiarContenido(id, contenido, demas, unejemplomas) {
+    if (document.getElementById(id).innerHTML) {
+        console.log(contenido + ' ' + demas + unejemplomas)
+    }
+}*/
