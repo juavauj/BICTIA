368e1b14d350ef9bf42ffce70f7579308faf93eb
Juan Castaneda <juavauj@gmail.com>
2020-01-23 12:00:03 -0500

    Nuevos ejemplos con function y for

--- Bictia 23-01-2020/javascript.js
+++ Bictia 23-01-2020/javascript.js
@@ -1,13 +1,33 @@
-function cambiarTitulo(id, otromensaje) {
-    document.getElementById(id).innerHTML = otromensaje;
-}
-
-for (var i = 0; i <= 10; i++)
+ function cambiarTitulo(id, otromensaje) {
+     document.getElementById(id).innerHTML = otromensaje;
+ }
 
 
 
-/*function cambiarContenido(id, contenido, demas, unejemplomas) {
-    if (document.getElementById(id).innerHTML) {
-        console.log(contenido + ' ' + demas + unejemplomas)
-    }
-}*/
+ function cambiarParrafos(arreglo) {
+
+     for (var i = 0; i < arreglo.length; i++) {
+         if (arreglo[i] == 'parrafo1') {
+             var contenido = 'Sí, este es mi parrafo 1'
+         } else {
+             var contenido = 'Este es el parrafo dos...'
+         }
+
+         document.getElementById(arreglo[i]).innerHTML = contenido
+     }
+ }
+
+ function cambiarNumeros(camN) {
+     for (var i = 1; i <= 10; i++) {
+         console.log("El número es: " + i)
+     }
+ }
+
+
+
+
+ /*function cambiarContenido(id, contenido, demas, unejemplomas) {
+     if (document.getElementById(id).innerHTML) {
+         console.log(contenido + ' ' + demas + unejemplomas)
+     }
+ }*/
