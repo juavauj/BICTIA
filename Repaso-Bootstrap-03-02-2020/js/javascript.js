2a3db678425c21da435ec0072cbc03c819798090
Juan Castaneda <juavauj@gmail.com>
2020-02-03 12:06:58 -0500

    Modificación de html con nuevo link a javascript, nuevas carpetas para css y js.

---
+++ Repaso-Bootstrap-03-02-2020/js/javascript.js
@@ -0,0 +1,25 @@
+var a = 1;
+const b = 2;
+
+function suma(a, b) {
+    console.log(a + b);
+}
+
+function mult(a, b) {
+    console.log(a * b);
+}
+
+function resta(a, b) {
+    console.log(a - b);
+}
+
+function divi(a, b) {
+    console.log(a / b);
+}
+
+function result(a, b) {
+    suma(a, b);
+    mult(a, b);
+    resta(a, b);
+    divi(a, b);
+}
