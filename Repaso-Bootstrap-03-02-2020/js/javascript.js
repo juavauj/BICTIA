6f80340ea1d627663d418a8d14b1392c4c4d2578
Juan Castaneda <juavauj@gmail.com>
2020-02-05 11:57:56 -0500

    Nuevas operaciones matemáticas en js. Ejercicios con for e if.

--- Repaso-Bootstrap-03-02-2020/js/javascript.js
+++ Repaso-Bootstrap-03-02-2020/js/javascript.js
@@ -1,5 +1,5 @@
-var a = 1;
-const b = 2;
+let a = 2;
+let b = 2;
 
 function suma(a, b) {
     console.log(a + b);
@@ -23,3 +23,37 @@ function result(a, b) {
     resta(a, b);
     divi(a, b);
 }
+
+
+// switch(numero) {
+//     case sum:
+//         suma(a, b);
+//         break;
+// }
+
+// var attemps = 0;
+// function getUser(){
+//     var user = localStorage.getItem('user');
+//     attemps += 1;
+//     if (attemps == 5){
+//         console.log('no se puede obtener el user');
+// } else{
+//     return user ? user : getUser();
+// }
+// }
+
+
+// Números primos del 0 al 10
+
+for (var num = 0; num <= 10; num++) {
+    if (num * num++ !== num) {
+        console.log(num);
+    }
+}
+
+// var s = 'Hola mundoffrhfhfhfh';
+// var l = s.length;
+
+// for (var numb = l; numb >= 0; numb--) {
+//     console.log(s[numb]);
+// }
