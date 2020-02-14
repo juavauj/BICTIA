f04fc68ef6ffaa6df5212e53f428adb311950658
Juan Castaneda <juavauj@gmail.com>
2020-02-14 09:51:18 -0500

    Nueva carpeta con repaso de JS.

---
+++ Repaso-JS-14-02-2020/JS/script.js
@@ -0,0 +1,24 @@
+// Ejercicios Javascript http://progra.usm.cl/apunte/ejercicios/index.html
+
+
+// Saludo
+
+// let nombre = prompt("Ingresa tu nombre: ");
+// alert(`Hola ${nombre}`);
+
+
+// Círculos
+// let radio = prompt('Ingrese el radio: ');
+// alert(`Area: ${Math.PI * (radio**2)}`);
+// alert(`Perimetro: ${2 * Math.PI * radio}`);
+
+
+// Promedio.
+
+
+// Longitud
+// let cm = prompt('Ingrese longitud: ');
+// console.log(`${cm} cm = ${cm / 2.54} in`);
+
+
+//Reverse
